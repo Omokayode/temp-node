ea292592fc9978dac712bbc7c257427adf25517c
John Omokayode <johnomokayode@Johns-MacBook-Air.local>
2022-08-02 16:18:47 +0100

    first commit

---
+++ 3-module.js
@@ -0,0 +1,15 @@
+//comMonJS, every file is a module (by default)
+//Modules - encapsulated code (only share minimum)
+
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alt-flavour')
+require('./7-mind-grenade')
+//console.log(data)
+//console.log(names)
+
+// sayHi('susan')
+// sayHi(names.John)
+// sayHi(names.peter)
+
+
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+//local
+const secret = 'SUPER SECRET'
+//share
+const John = 'john'
+const peter = 'peter'
+
+module.exports = { John, peter} 
